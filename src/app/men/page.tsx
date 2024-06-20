95410e64dbe5fb825e2f1bf1553c190804bf520e
Henry Agu <aguhenrychuks@gmail.com>
2024-06-20 22:59:59 +0100

    components in men page route

--- src/app/men/page.tsx
+++ src/app/men/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../components/ui/Navbar/Navbar'
+import Footer from '../components/ui/Footer/Footer'
 
 const page = () => {
   return (
     <div>
+        <Navbar/>
       <h1>Men o</h1>
+      <Footer/>
     </div>
   )
 }
