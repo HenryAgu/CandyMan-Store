65c0ab53373a1830f80d0a17824aeb79bf510ef3
Henry Agu <aguhenrychuks@gmail.com>
2024-08-15 17:26:59 +0100

    fix: header

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
 			<main className="">
 				<Navbar />
 				<div className="mx-auto px-0 container">
-					<div className="bg-[url('/assets/MobileShop.png')] bg-center bg-cover bg-no-repeat lg:bg-[url('/assets/Hero.png')] h-screen w-full">
+					<div className="bg-[url('/assets/MobileShop.png')] bg-center bg-cover bg-no-repeat lg:bg-[url('/assets/Hero.png')] h-[800px] w-full">
 						<div className="flex flex-col gap-y-3.5 text-white lg:p-40 lg:py-48 pt-60 w-[90%] mx-auto lg:mx-0 lg:w-fit text-center">
 							<h3 className="font-normal font-inter text-5xl">Your Cozy Era</h3>
 							<p className="font-normal font-inter text-2xl">
